8c2366d7b1e2d3a9fb226a81b89e625d1a8ee2f9
simadad <simadad@sina.com>
2017-05-24 16:21:51 +0800

    create plan

---
+++ personal_plan.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python3.5
+
+
+def search_students():
+    """
+    搜索并返回符合条件的学生用户名，生成器形式
+    :return:
+    """
+
+
+def search_wechat():
+    """
+    接受信息，返回微信号
+    :return:
+    """
+
+
+def comb_info():
+    """
+    组合信息，返回消息模版
+    :return:
+    """
+
+
+def send_msg():
+    """
+    发送消息
+    :return:
+    """
+
+
+def run():
+    """
+    search_students -> search_wechat -> comb_info -> send_msg
+    :return:
+    """
+
+if __name__ == '__main__':
+    run()
