a7c9dce3e732375f6c09a1c4c91c099d830274f8
simadad <simadad@sina.com>
2017-06-22 15:10:53 +0800

    graph weekly

---
+++ run_weekly.py
@@ -0,0 +1,41 @@
+import itchat
+import os
+import shutil
+import datetime
+from personal_plan import log_this
+from graph import graph_dir, graph_send_dir, log_path
+
+itchat.auto_login(hotReload=True)
+
+
+@log_this
+def send_graph():
+    cwd = os.getcwd()
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    root_dir = cwd + '/' + graph_dir
+    with open(log_path, 'a', encoding='utf8') as f:
+        f.write('\n{now}:\n'.format(now=now))
+        for name in os.listdir(root_dir):
+            rooms = itchat.search_chatrooms(name=name)
+            username = rooms[0]['UserName']
+            name_dir = root_dir + '/' + name
+            for file in os.listdir(name_dir):
+                img_path = name_dir + '/' + file
+                if os.path.isfile(img_path):
+                    # itchat.send('@img@%s' % img_path, username)       # TODO del #
+                    f.write('{img:<60}SEND!\n'.format(img=file))
+                    send_dir = name_dir + '/' + graph_send_dir + '/' + file
+                    try:
+                        shutil.move(img_path, send_dir)
+                    except FileNotFoundError:
+                        os.mkdir(name_dir + '/' + graph_send_dir)
+                        shutil.move(img_path, send_dir)
+                    f.write('{img:<60}MOVED!\n'.format(img=file))
+        f.write('\n')
+
+
+def run():
+    send_graph()
+
+if __name__ == '__main__':
+    run()
